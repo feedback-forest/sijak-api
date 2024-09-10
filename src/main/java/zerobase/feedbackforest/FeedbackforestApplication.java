f9e3cf3067c7d622755e264f58ffc78863147df3
YongBum <dt10002@naver.com>
2024-09-10 18:15:56 +0900

    init directory structure

---
+++ src/main/java/zerobase/feedbackforest/FeedbackforestApplication.java
@@ -0,0 +1,13 @@
+package zerobase.feedbackforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackforestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbackforestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zerobase/feedbackforest/FeedbackforestApplicationTests.java
@@ -0,0 +1,13 @@
+package zerobase.feedbackforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackforestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
