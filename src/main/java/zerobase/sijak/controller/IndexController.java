52bdb80ec2c04ea6f973231986274b24cc553d6c
YongBum <dt10002@naver.com>
2024-10-03 17:23:56 +0900

    Fix: env info update

--- src/main/java/zerobase/sijak/controller/IndexController.java
+++ src/main/java/zerobase/sijak/controller/IndexController.java
@@ -1,13 +1,23 @@
 package zerobase.sijak.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    @Value("${client-id}")
+    private String CLIENT_ID;
+
+    @Value("${redirect-uri}")
+    private String REDIRECT_URI;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("CLIENT_ID", CLIENT_ID);
+        model.addAttribute("REDIRECT_URI", REDIRECT_URI);
         return "index";
     }
 
