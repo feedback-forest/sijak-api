ecce573604d38eb308b543c87eef6be46f54dc66
YongBum <dt10002@naver.com>
2024-10-12 20:20:33 +0900

    Refactor: HeartService 수정 & 크롤링 수정

--- src/main/java/zerobase/sijak/service/scrap/NowonScrapService.java
+++ src/main/java/zerobase/sijak/service/scrap/NowonScrapService.java
@@ -307,11 +307,16 @@ public class NowonScrapService {
         Lecture lecture = lectureRepository.findByLink(link);
 
         if (lecture == null) return false;
-        else if (lecture.isStatus() && !lectureStatus.trim().equals("수강신청")) {
+        else if (!lecture.isStatus() && lectureStatus.trim().equals("수강신청")) {
+            lecture.setStatus(true);
+            lectureRepository.save(lecture);
+            return false;
+        } else if (lecture.isStatus() && !lectureStatus.trim().equals("수강신청")) {
             lecture.setStatus(false);
             lectureRepository.save(lecture);
             return true;
         }
+
         return true;
     }
 }
